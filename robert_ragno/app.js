ee32a02229a1c1fc5b2c003700fe8703fa58ca25
rragno <robertragno@gmail.com>
2014-10-07 09:38:05 -0700

    Put clickhandler in it's own named function

--- robert_ragno/app.js
+++ robert_ragno/app.js
@@ -1,28 +1,27 @@
-//clicking on each table td changes div class from "on" to "off"
 var cells = document.getElementsByTagName("td");
 var counter = 0;
 
-for (var i = 0; i < cells.length; i++) {
-    cells[i].addEventListener("click", function() {
-    	//Check that cell is "off" and that it hasn't been clicked yet
+//Check that cell is "off" and that it hasn't been clicked yet
+var clickhandler = function() {
     	if (counter % 2 === 0 && this.innerHTML === "") {
 	    	this.innerHTML = "X";
 	    	this.setAttribute("style", "background-color:blue;");
-
 	    	counter++;
 	    }
 	    else if (this.innerHTML === "") {
 	  		this.innerHTML = "O";
 	  		this.setAttribute("style", "background-color:red;");
-
 	  		counter++;
 	    }
-    });
+}
+
+//Assigns clickhandler function to each td cell
+for (var i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", clickhandler);
 };
 
 //Reset game board
 document.getElementsByTagName("button")[0].addEventListener("click", function() {
-	console.log("HI");
 	for (i = 0; i < cells.length; i++) {
 		cells[i].innerHTML = "";
 		cells[i].setAttribute("style", "background-color:#262626;");
