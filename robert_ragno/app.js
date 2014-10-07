a0c99670f57f85a2555b95be0f18cc4da21c6435
rragno <robertragno@gmail.com>
2014-10-07 09:15:10 -0700

    Regular req and one bonus completed

--- robert_ragno/app.js
+++ robert_ragno/app.js
@@ -1,1 +1,30 @@
-//Your code here
+//clicking on each table td changes div class from "on" to "off"
+var cells = document.getElementsByTagName("td");
+var counter = 0;
+
+for (var i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", function() {
+    	//Check that cell is "off" and that it hasn't been clicked yet
+    	if (counter % 2 === 0 && this.innerHTML === "") {
+	    	this.innerHTML = "X";
+	    	this.setAttribute("style", "background-color:blue;");
+
+	    	counter++;
+	    }
+	    else if (this.innerHTML === "") {
+	  		this.innerHTML = "O";
+	  		this.setAttribute("style", "background-color:red;");
+
+	  		counter++;
+	    }
+    });
+};
+
+//Reset game board
+document.getElementsByTagName("button")[0].addEventListener("click", function() {
+	console.log("HI");
+	for (i = 0; i < cells.length; i++) {
+		cells[i].innerHTML = "";
+		cells[i].setAttribute("style", "background-color:#262626;");
+	}
+});
